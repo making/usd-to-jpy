0ec709f0e9ea555edf18aa973db13f2673238a98
Toshiaki Maki <makingx@gmail.com>
2024-03-25 13:15:12 +0900

    update

--- scrape.sh
+++ scrape.sh
@@ -30,10 +30,10 @@ dump() {
 }
 
 # Starting date
-start_date="2023-12-23"
+start_date="2024-03-10"
 
 # Ending date
-end_date="2024-03-10"
+end_date="2024-03-24"
 
 # Iterate over each day
 current_date=$start_date
